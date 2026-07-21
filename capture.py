f5c8a43aa847785bd76b671de0dbde8a1163bf90
Sebas0317 <sebastiansandoval12371@gmail.com>
2026-07-21 00:04:50 -0500

    Remove stale test files

--- capture.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-import pyautogui
-
-img = pyautogui.screenshot()
-img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-cv2.imwrite("captured.png", img)
-print(f"Capturada! ({img.shape[1]}x{img.shape[0]})")
